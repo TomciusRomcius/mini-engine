3dc8bebed3d78605a28041f0a2eee5dd9b94211e
Tomas Rimkevicius <tomas.ew88@gmail.com>
2026-09-12 17:18:33 +0300

    Set up pch.h

---
+++ pch.h
@@ -0,0 +1,29 @@
+#pragma once
+
+// Standard library
+#include <algorithm>
+#include <cmath>
+#include <cstdint>
+#include <fstream>
+#include <iostream>
+#include <list>
+#include <memory>
+#include <numeric>
+#include <sstream>
+#include <stdexcept>
+#include <string>
+#include <type_traits>
+#include <unordered_map>
+#include <vector>
+
+// Third-party
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
+#include <imgui.h>
+#include <imgui_impl_glfw.h>
+#include <imgui_impl_opengl3.h>
